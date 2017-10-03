91d519a4858d628d2c546ed99a4ad55c19cda713
U-SLIPPERY\alex <C:\Soft\TheBat\Mail>
2017-10-03 12:48:29 +0200

    added a draft implementation of checks that involve sending and receiving an expected message

--- cabot_check_network/models.py
+++ cabot_check_network/models.py
@@ -17,17 +17,56 @@ class NetworkStatusCheck(StatusCheck):
         help_text='Port to check.',
     )
 
+    # if set, this message will be sent to the server after the connection
+    # is established
+    message_to_send = None
+    message_to_send_b64 = False
+
+    # if set, we shall expect the server to respond with this message, for
+    # the test to be considered successful
+    expected_reply = None
+    expected_reply_b64 = False
+
+
+    # General note on the format of these messages. Typically you want to
+    # send a string, but for some protocols you might want to send a binary
+    # message consisting of non-printing characters. If that is your case,
+    # encode the strings in base64, they will be decoded and used further
+    # in the check
+
     def _run(self):
         result = StatusCheckResult(status_check=self)
 
         try:
             s = socket.create_connection((self.host, self.port), self.timeout)
-            s.shutdown(socket.SHUT_RDWR)
-            s.close()
         except Exception as e:
             result.error = u'Error occurred: %s' % (e.message,)
             result.succeeded = False
         else:
+            # the connection was successful, refine the check by verifying
+            # if there are any other success criteria and if they are satisfied
+
+
+            # here we verify whether the check consists of sending a message to
+            # the server or not, and whether it is a binay payload or not
+            if self.message_to_send:
+                if self. message_to_send_b64:
+                    self.message_to_send = self.message_to_send.decode('base64')
+                s.send(self.message_to_send)
+
+            if self.expected_reply:
+                if self.expected_reply_b64:
+                    self.expected_reply = self.expected_reply.decode('base64')
+
+                received_response = s.read()
+                if received_response == self.expected_reply:
+                    result.succeeded = True
+                else:
+                    result.succeeded = False
+
             result.succeeded = True
+        finally:
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
 
         return result
